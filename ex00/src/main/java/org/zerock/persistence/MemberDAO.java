dbde3b840b718e4fbf81330c63a09b38f08d9a23
DaeJin Jeong <devwithu@naver.com>
2018-01-09 07:28:03 +0900

    Chapter 6

--- ex00/src/main/java/org/zerock/persistence/MemberDAO.java
+++ ex00/src/main/java/org/zerock/persistence/MemberDAO.java
@@ -6,5 +6,7 @@ public interface MemberDAO {
 	
 	public String getTime();
 	public void insertMember(MemberVO vo);
+	public MemberVO readMember(String userid)throws Exception;
+	public MemberVO readWithPW(String userid, String pw)throws Exception;
 
 }
